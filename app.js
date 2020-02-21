59ccd81577016eaa5d86f247f101db5f58b13c46
kkitko808 <dento85@tutamail.com>
2020-02-21 12:33:17 -0600

    changed the classes to inherit from employee rather than be stand alone classes

--- app.js
+++ app.js
@@ -8,37 +8,39 @@ const fs = require("fs")
 inquirer
     .prompt([
         {
-            name: ("name"),
-            type: ("input"),
-            message: ("What is the name of the Employee?")
+            name: "name",
+            type: "input",
+            message: "What is the name of the Employee?"
         },
         {
-            name: ("id"),
-            type: ("input"),
-            message: ("What is the Employee's ID")
+            name: "id",
+            type: "input",
+            message: "What is the Employee's ID"
         },
         {
-            name: ("email"),
-            type: ("input"),
-            message: ("What is the Employee's email")
+            name: "email",
+            type: "input",
+            message: "What is the Employee's email"
         },
         {
-            name: ("title"),
-            type: ("list"),
-            choices: [Manager, Employee, Intern, Engineer]
+            name: "title",
+            type: "list",
+            choices: [Manager, Intern, Engineer]
         }
     ])
     .then(response => {
+
         let empName = response.name;
         let empId = response.id;
         let empEmail = response.email;
         let empTitle = response.title;
+
         fs.readFile("templates/employee.html", 'utf8', (err, data) => {
             if (err) throw err;
             data = data.replace("{name}", empName)
             data = data.replace("{id}", empId)
             data = data.replace("{email}", empEmail)
             data = data.replace("{title}", empTitle)
-            console.log(data)
+
         })
     });
